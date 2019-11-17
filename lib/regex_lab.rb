c30fba5a931597a283cf3fc2d5f7562f17a7e043
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-17 23:41:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,17 +7,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\bun(\w*)ing/)
+  text.scan(/\bun(\w*)ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(//)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(//)
 end
 
 def valid_phone_number?(phone)
-
+ phone.scan(//)
 end

2b4c44132564d4a18ec814193bd80ea08d4147ac
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-18 01:03:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,5 +19,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- p phone.scan(/(\d{3})\W?(\d{3})\W?(\d{4})/)
+ return true if phone.scan(/(\d{3})\W?(\d{3})\W?(\d{4})/).all?
 end
